2efd0b5f52215363c5e02bd528e19fa965854f3c
Eugerico Caranto <eugericocarl@gmail.com>
2025-07-03 11:18:04 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-2xl bg-black text-white p-8 text-center'>Hello react!</h1>
+    </>
+  )
+}
+
+export default App
