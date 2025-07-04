0ed8b85a46b7ae9569bd51ef575c66e3c66d138b
Eugerico Caranto <eugericocarl@gmail.com>
2025-07-04 15:58:59 +0800

    added accordion with video

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import CompanyLogo from './components/CompanyLogo'
+import DesignSection from './components/DesignSection'
 import FeaturesSection from './components/FeaturesSection'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
@@ -12,6 +13,7 @@ function App() {
     <Hero/>
     <CompanyLogo/>
     <FeaturesSection/>
+    <DesignSection/>
     </>
   )
 }
