ed7ccc8c6f4eb4b975fceabc135457b9256a1d2b
Eugerico Caranto <eugericocarl@gmail.com>
2025-07-04 10:50:39 +0800

    finish navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-    <h1 className='text-2xl bg-black text-white p-8 text-center'>Hello react!</h1>
+    <Navbar/>
     </>
   )
 }
