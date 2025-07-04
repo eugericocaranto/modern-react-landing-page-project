b13a41872e50a349fb0a0af57dc82dc6e37c8a9b
Eugerico Caranto <eugericocarl@gmail.com>
2025-07-04 11:55:00 +0800

    add company logos loop

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import CompanyLogo from './components/CompanyLogo'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <Navbar/>
     <Hero/>
+    <CompanyLogo/>
     </>
   )
 }
