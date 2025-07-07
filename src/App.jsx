8f5288a0dbcfeb53c4d354386c0faa844caab429
Eugerico Caranto <eugericocarl@gmail.com>
2025-07-07 12:02:36 +0800

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import CompanyLogo from './components/CompanyLogo'
 import CustomerSection from './components/CustomerSection'
 import DesignSection from './components/DesignSection'
 import FeaturesSection from './components/FeaturesSection'
+import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import TryNow from './components/TryNow'
@@ -20,6 +21,7 @@ function App() {
     <CustomerSection/>
     <AboutSection/>
     <TryNow/>
+    <Footer/>
     </>
   )
 }
